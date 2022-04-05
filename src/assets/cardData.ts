62ce9e98c12dec1ee2ebfb029273133ab8838120
leninner <mazabandalenin180@gmail.com>
2022-04-05 11:44:06 -0500

    Initial commit

---
+++ src/assets/cardData.ts
@@ -0,0 +1,17 @@
+export const cardsData = [
+  { id: 1, img: 'https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080194-7382724028-unkno.png' },
+  { id: 2, img: 'https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080203-3155899631-unkno.png' },
+  { id: 3, img: 'https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080201-7164581366-unkno.png' },
+  { id: 4, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434111-473592_sanji_time_skip_1.png' },
+  { id: 5, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434105-render_nicorobin.png' },
+  { id: 6, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434113-chopper_time_skip_2.png' },
+  { id: 7, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434115-473629_untitled_2.png' },
+  { id: 8, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2434117-473497_brook_timeskip1.png' },
+  { id: 9, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/2841730-law.jpg' },
+  { id: 10, img: 'https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7078671-9363995852-39fc3.jpg' },
+  { id: 11, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/47703/3056239-7978640494-Jinbe.png' },
+  { id: 12, img: 'https://comicvine.gamespot.com/a/uploads/square_small/4/46706/1152176-current_blackbeard.png' },
+  { id: 13, img: 'https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080183-4615055614-unkno.png' },
+  { id: 14, img: 'https://comicvine.gamespot.com/a/uploads/square_small/5/57216/1090020-61600.jpg' },
+  { id: 15, img: 'https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080202-1635462455-unkno.png' },
+];
